5c61d88d38b684635532c01323b1e712c28709fa
Estelle Quezel <estellequezel@gmail.com>
2020-02-20 17:56:37 -0300

    Back end working, front end WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "Cachaça")
+Ingredient.create(name: "Ginger Ale")
+Ingredient.create(name: "tomato juice")
+Ingredient.create(name: "tabasco")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "tonic")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Caipirinha")
+Cocktail.create(name: "Caipiroska")
+Cocktail.create(name: "Moscow Mule")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Gin Tonic")
